dce77f39eb8196908f0af83ca8688a2c6656a7eb
Namesh Kher <namesh.kher@gmail.com>
2016-04-03 21:44:16 -0700

    Adding BST data structure

---
+++ src/datastructures/bst.go
@@ -0,0 +1,58 @@
+package datastructures
+
+import "fmt"
+
+type BstNode struct {
+	Left *BstNode
+	Right *BstNode
+	Data int
+}
+
+func Insert(root *BstNode, data int) *BstNode {
+	if root == nil {
+		return &BstNode{nil, data, nil}
+	}
+	if data < root.Data {
+		root.Left = Insert(root.Left, data)
+		return root
+	} else {
+		root.Right = Insert(root.Right, data)
+		return root
+	}
+}
+
+func Height(root *BstNode) int {
+	if root == nil {
+		return 0
+	}
+	leftHeight := Height(root.Left)
+	rightHeight := Height(root.Right)
+	if leftHeight > rightHeight {
+		return leftHeight + 1
+	}
+	return rightHeight + 1
+}
+
+func Inorder(root *BstNode) {
+	if root != nil {
+		Inorder(root.Left)
+		fmt.Print(root.Data, " ")
+		Inorder(root.Right)
+	}
+}
+
+func Preorder(root *BstNode) {
+	if root != nil {
+		fmt.Print(root.Data, " ")
+		Preorder(root.Left)
+		Preorder(root.Right)
+	}
+}
+
+func Postorder(root *BstNode) {
+	if root != nil {
+		Postorder(root.Left)
+		Postorder(root.Right)
+		fmt.Print(root.Data, " ")
+	}
+}
