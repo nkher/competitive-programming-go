3cc446ac834e0f79e8a3ebeed5400673d7d768b5
Namesh Kher <namesh.kher@gmail.com>
2016-04-07 00:04:43 -0700

    Committing a new Fresh Golang Repo

---
+++ trees/dividebinarytreetwohalves.go
@@ -0,0 +1,110 @@
+// +build XXX
+
+package main 
+
+import (
+	"fmt"
+	"datastructures"
+)
+
+/** Solution 1 - Brute Force - O(N^2) */
+func checkRecursive(node *bst.BstNode, n int) bool {
+	
+	if node == nil {
+		return false
+	}
+	if bst.Size(node) == n-bst.Size(node) {
+		return true
+	}
+	return checkRecursive(node.Left, n) || checkRecursive(node.Right, n)
+}
+
+func checkBinaryTreeEqualSizedDivision(root *bst.BstNode) bool {
+	size := bst.Size(root)
+	return checkRecursive(root, size)
+}
+
+/** Solution 2 - O(N) */
+
+func checkRecursiveEfficient(node *bst.BstNode, n int, result *bool) int {
+	
+	if node == nil {
+		return 0
+	}
+	
+	// Calculate the sizes of the left and right child
+	c := checkRecursiveEfficient(node.Left, n, result) + 1 + 
+			checkRecursiveEfficient(node.Right, n, result)
+	
+	// Check if tree can be divided or not
+	if c == n-c {
+		*result = true 
+	}
+	
+	return c
+}
+
+func checkBinaryTreeEqualSizedDivisionEfficient(root *bst.BstNode) bool {
+	size := bst.Size(root)
+	result := false
+	checkRecursiveEfficient(root, size, &result)
+	return result
+}
+
+func main() {
+	
+	/** Using BST to create a binary tree */
+	var root *bst.BstNode
+	root = bst.Insert(root, 19)
+  	root = bst.Insert(root, 7)
+  	root = bst.Insert(root, 3)
+  	root = bst.Insert(root, 23)
+  	root = bst.Insert(root, 20)
+  	root = bst.Insert(root, 30)
+  	
+  	fmt.Println("\n--------Using BruteForce Algorithm------------\n")
+  	
+  	result := checkBinaryTreeEqualSizedDivision(root)
+  	
+  	if result == true {
+  		fmt.Println("Binary Tree can be divided into 2 equal halves.")
+  	} else {
+  		fmt.Println("Binary Tree cannot be divided into 2 equal halves.")
+  	}
+  	
+  	var root1 *bst.BstNode
+	root1 = bst.Insert(root1, 10)
+  	root1 = bst.Insert(root1, 1)
+  	root1 = bst.Insert(root1, 30)
+  	root1 = bst.Insert(root1, 25)
+  	root1 = bst.Insert(root1, 20)
+  	root1 = bst.Insert(root1, 28)
+  	root1 = bst.Insert(root1, 40)
+  	root1 = bst.Insert(root1, 50)
+  	
+  	result = checkBinaryTreeEqualSizedDivision(root1)
+  	if result == true {
+  		fmt.Println("Binary Tree can be divided into 2 equal halves.")
+  	} else {
+  		fmt.Println("Binary Tree cannot be divided into 2 equal halves.")
+  	}
+  	
+  	fmt.Println("\n--------Using Efficient Algorithm------------\n")
+  	
+  	result = checkBinaryTreeEqualSizedDivisionEfficient(root)
+    if result == true {
+  		fmt.Println("Binary Tree can be divided into 2 equal halves.")
+  	} else {
+  		fmt.Println("Binary Tree cannot be divided into 2 equal halves.")
+  	}
+  	
+  	result = checkBinaryTreeEqualSizedDivisionEfficient(root1)
+    if result == true {
+  		fmt.Println("Binary Tree can be divided into 2 equal halves.")
+  	} else {
+  		fmt.Println("Binary Tree cannot be divided into 2 equal halves.\n")
+  	}
+  	
+  	
+}
+
---
+++ trees/sumcoveredandunconvered.go
@@ -0,0 +1,86 @@
+// +build XXX
+
+package main 
+
+import (
+	"fmt"
+	"datastructures"
+)
+
+func getSumOfUncoveredNodesLeft(node *bst.BstNode) int {
+	
+	if node.Left == nil && node.Right == nil {
+		return node.Data
+	}
+	
+	if node.Left != nil {
+		return node.Data + getSumOfUncoveredNodesLeft(node.Left)
+	}
+	return node.Data + getSumOfUncoveredNodesLeft(node.Right)
+}
+
+func getSumOfUncoveredNodesRight(node *bst.BstNode) int {
+	
+	if node.Right == nil && node.Left == nil {
+		return node.Data
+	}
+	
+	if node.Right != nil {
+		return node.Data + getSumOfUncoveredNodesRight(node.Right)
+	}
+	return node.Data + getSumOfUncoveredNodesRight(node.Left)
+}
+
+func getSumOfUncoveredElements(root *bst.BstNode) int {
+	
+	leftBoundary, rightBoundary := 0, 0
+	
+	if root.Left != nil {
+		leftBoundary = getSumOfUncoveredNodesLeft(root.Left)
+	}
+	
+	if  root.Right != nil {
+		rightBoundary = getSumOfUncoveredNodesRight(root.Right)
+	}
+	
+	return (root.Data + leftBoundary + rightBoundary)
+}
+
+func getSumOfTree(root *bst.BstNode) int {
+	if root == nil {
+		return 0
+	}
+	return root.Data + getSumOfTree(root.Left) + getSumOfTree(root.Right)
+}
+
+func checkIfSameSum(root *bst.BstNode) bool {
+	
+	uncoveredSum := getSumOfUncoveredElements(root)
+	sumOfTree := getSumOfTree(root)
+	
+	return (sumOfTree == (sumOfTree - uncoveredSum))
+}
+
+func main() {
+	
+	/** Using BST to create a binary tree */
+	var root *bst.BstNode
+	root = bst.Insert(root, 8)
+  	root = bst.Insert(root, 10)
+  	root = bst.Insert(root, 14)
+  	root = bst.Insert(root, 13)
+  	root = bst.Insert(root, 3)
+  	root = bst.Insert(root, 1)
+  	root = bst.Insert(root, 6)
+  	root = bst.Insert(root, 4)
+  	root = bst.Insert(root, 7)
+  	
+  	result := checkIfSameSum(root)
+  	
+  	if result == true {
+  		fmt.Println("Sum of covered and uncovered is same\n")
+  	} else {
+  		fmt.Println("Sum of covered and uncovered is not the same\n")
+  	}
+}
+
